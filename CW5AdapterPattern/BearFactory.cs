1dc10eab0b29f030c8aef9f873d467fc89e5306e
Victor Gasior <vicagasi@ut.utm.edu>
2022-02-08 19:50:35 -0600

    Add project files.

---
+++ CW5AdapterPattern/BearFactory.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CW5AdapterPattern
+{
+    class BearFactory
+    {
+        abstract public class Bear{
+            abstract public void maul();
+            abstract public void hibernate();
+        }
+
+        abstract public class ToyBear
+        {
+            abstract public void hug();
+        }
+
+        public class Grizzly : Bear
+        {
+            override public void maul()
+            {
+                Console.WriteLine("The customer was mauled by the bear");
+            }
+            override public void hibernate()
+            {
+                Console.WriteLine("The customer is bored because the bear hibernating");
+            }
+        }
+
+        public class TeddyBear : ToyBear
+        {
+            override public void hug()
+            {
+                Console.WriteLine("The customer hugs the teddy bear!");
+            }
+        }
+
+        public class BearAdapter
+        {
+            private readonly Grizzly grizzly;
+            public BearAdapter(Grizzly grizzly)
+            {
+                this.grizzly = grizzly;
+            }
+
+            public void hug()
+            {
+                this.grizzly.maul();
+            }
+        }
+    }
+}
---
+++ CW5AdapterPattern/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CW5AdapterPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BearFactory.Grizzly grizzly = new BearFactory.Grizzly();
+            BearFactory.TeddyBear teddy = new BearFactory.TeddyBear();
+            BearFactory.BearAdapter adaptedTed = new BearFactory.BearAdapter(grizzly);
+
+            grizzly.maul();
+            grizzly.hibernate();
+
+            teddy.hug();
+
+            adaptedTed.hug();
+        }
+    }
+}
